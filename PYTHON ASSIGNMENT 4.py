9c898034717861253024ec91f31e49bf71e97d28
diviksatija <97514653+diviksatija@users.noreply.github.com>
2022-03-13 10:18:50 +0530

    Update PYTHON ASSIGNMENT 4.py

--- PYTHON ASSIGNMENT 4.py
+++ PYTHON ASSIGNMENT 4.py
@@ -139,37 +139,26 @@ print("\n")
 
 #QUESTION 6
 
-#inputting a word from the first friend
-word =input("Enter the word: ")
-word=word.lower()
-
-#inputting a meaningful word with the exact same letters
-testword = input("Enter a new meaningful word using the exact same letters to test your friendship: ")
-testword=testword.lower()
-#defining dictionary
-def count_in_dict(word):
-    count = {}
-    list1 = list(word)
-    
-    n = len(list1)
-    for i in range(n):
-        if list1[i] in count:
-            count[list1[i]] += 1
-        else:
-            count[list1[i]] = 1
-    return count
-#shopkeeper's input to verify the word's meaning
-def userinput():
-    if count_in_dict(word) != count_in_dict(testword):
-        print("The letters aren't exact, friendship is fake")
-        return
-    ans = input("Does the word makes sense?(y/Y or n/N )")
-
-    if ans == 'y' or ans=='Y':
-        print("The friends pass the friendship test")
-    elif ans == 'n' or ans=='N':
-        print("The word doesn't have a meaning, friendship is fake")
+a=str(input("George's word."))
+a=a.lower()
+b=str(input("Barbie's word. "))
+b=b.lower()
+list1=[]
+list2=[]
+for i in range(len(a)):
+    list1.append(a[i:i+1])
+for i in range(len(b)):
+    list2.append(b[i:i+1])
+list1=sorted(list1)
+list2=sorted(list2)
+if len(a)!=len(b):
+    print("Friendship is fake.")
+else:
+    count=0
+    for i in range(len(a)):
+        if list1[i]==list2[i]:
+            count=count+1
+    if count==len(list1):
+        print("Friendship is true.")
     else:
-        print("Invalid input,try again with y/Y or n/N ")
-        userinput()
-userinput()
+        print("Friendship is fake.")
